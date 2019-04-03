079d94f9f758385216c870561bdae41720e4f5d7
jesus <imangelo@hotmail.es>
2019-04-04 00:34:35 +0200

    diseño de la pagina de busqueda pelicula

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  textoBuscar: string = '';
+  ideas: string[] = ['uno', 'dos', 'tres'];
+  buscar(e){
+    const valor = ""
+  }
+}
